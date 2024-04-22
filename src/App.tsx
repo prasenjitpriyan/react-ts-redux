5eb97d025e88e33159f8c96962fc4854dee2262f
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-22 18:07:07 +0530

    Initial setup complete

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-950 min-h-screen flex justify-center items-center text-white">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
